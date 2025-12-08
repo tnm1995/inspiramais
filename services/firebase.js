d1a64f3ff84a3df3be3e03fede1c825565a5db8f
tnm1995 <thalesnunes-10@hotmail.com>
2025-12-08 14:01:03 -0300

    Firebase env variables + Vercel fix

--- services/firebase.js
+++ services/firebase.js
@@ -1,23 +1,31 @@
-// src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
+echo import { initializeApp } from "firebase/app";^
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDmqsdfWwUVK5koxlOnW_-k_TsMLGVCQ7o",
-  authDomain: "inspiramais-6f73b.firebaseapp.com",
-  projectId: "inspiramais-6f73b",
-  storageBucket: "inspiramais-6f73b.firebasestorage.app",
-  messagingSenderId: "747123918474",
-  appId: "1:747123918474:web:bd55b098fd2f2c2ec0cf73",
-  measurementId: "G-EBE8DRQ48E"
-};
+import { getFirestore } from "firebase/firestore";^
 
-// Inicializa o Firebase
-const app = initializeApp(firebaseConfig);
+import { getAuth } from "firebase/auth";^
 
-// Exporta os serviços que você vai usar
-export const db = getFirestore(app);        // ← Banco de dados (Firestore)
-export const auth = getAuth(app);           // ← Login (Google, email, etc)
-export const analytics = getAnalytics(app); // ← Opcional (pode apagar se não usar)
+^
+
+const firebaseConfig = {^
+
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,^
+
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,^
+
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,^
+
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,^
+
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,^
+
+  appId: import.meta.env.VITE_FIREBASE_APP_ID^
+
+};^
+
+^
+
+const app = initializeApp(firebaseConfig);^
+
+export const db = getFirestore(app);^
+
+export const auth = getAuth(app); > services\firebase.js
